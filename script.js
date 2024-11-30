9900b625f772a71b1bd1a040e7044817129775f2
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-30 17:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-//your code here!
+
+var list = document.getElementById('infi-list');
+
+// Add event listener for scroll event
+list.addEventListener('scroll', function() {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+   
+    addMoreItems();
+  }
+});
+
+
+function addMoreItems() {
+  for (var i = 0; i < 2; i++) {
+    var newItem = document.createElement('li');
+    newItem.textContent = 'Item ' + (list.childElementCount + 1);
+    list.appendChild(newItem);
+  }
+}
 
